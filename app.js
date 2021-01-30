6378913cae425f8cc52880046d686e3b489c86e2
Zaid Sohel Ajani <zaidajani2007@gmail.com>
2021-01-30 09:35:53 +0530

    Remove comments

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
 const path = require('path');
-
 const app = express();
 
-// Passport Config
 require('./config/passport')(passport);
 
-// DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
 mongoose
   .connect(
     db,
@@ -23,14 +19,11 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Express body parser
 app.use(express.urlencoded({ extended: true }));
 
-// Express session
 app.use(
   session({
     secret: 'secret',
@@ -39,14 +32,9 @@ app.use(
   })
 );
 
-// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Connect flash
 app.use(flash());
-
-// Global variables
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
@@ -54,7 +42,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 app.use('/static', express.static(path.join(__dirname + '/static')));
